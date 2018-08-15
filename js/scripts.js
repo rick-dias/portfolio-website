342f2946c272ba96e3cc41c94e0843f5f2413843
rick-dias <41041000+rick-dias@users.noreply.github.com>
2018-08-15 11:07:03 -0700

    Add files via upload
    add rollover effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("hello,welcome to Ang's site")
+
+$(document).ready(function() {
+  $(".project").on("mouseenter", function() {
+    $(this).find(".project-details").fadeIn();
+    });
+  $(".project").on("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+    });
+});
